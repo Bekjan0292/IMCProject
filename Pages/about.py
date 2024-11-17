75c0df513c284bad21b09f8225b185df5ae2d828
Bekjan0292 <166012227+Bekjan0292@users.noreply.github.com>
2024-11-17 19:14:03 +0900

    Delete Pages directory

--- Pages/about.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def about_page():
-    st.title("About")
-    st.markdown("""
-    ### About This Application
-    This Stock Analysis Application was developed to simplify stock research and help you make informed investment decisions.
-    """)
